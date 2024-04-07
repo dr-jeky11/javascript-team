777b77137a448d4020e987862ffd2feadcdb47b7
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-07 17:04:22 +0300

    add task-16

---
+++ js/task-16.js
@@ -0,0 +1,24 @@
+/**
+ *? Напишіть ф-цію calcTotalPrice(stones, stonesName),
+ *? яка приймає масив об'єктів та
+ *? рядок під назвою каменю.
+ *? Функція вважає та повертає загальну вартість каменів
+ *? з таким ім'ям, ціною та кількістю з об'єкта
+ */
+const stones = [
+  { name: "Смарагд", price: 1300, quantity: 4 },
+  { name: "Діамант", price: 2700, quantity: 6 },
+  { name: "Сапфір", price: 400, quantity: 7 },
+  { name: "Щебінь", price: 150, quantity: 100 },
+];
+
+function calcTotalPrice(stones, stonesName) {
+    for (const stone of stones) {
+        if (stonesName === stone.name) {
+            return stone.price * stone.quantity;
+        } 
+    }
+    return `Not found`;
+}
+
+console.log(calcTotalPrice(stones, "Щебінь"));
